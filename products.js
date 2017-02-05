d1e04f62baaaacda1232e74bbf36792bd217bc5d
Falk Hoppe <falkhoppe81@gmail.com>
2017-02-05 10:01:26 +0100

    fix product initialization

--- products.js
+++ products.js
@@ -1,8 +1,8 @@
 const YANG = require('yet-another-name-generator')
 const lorem = require('lorem-ipsum')
 
-const pages = new Array(10, null) // eslint-disable-line no-array-constructor
-const entries = new Array(10, null) // eslint-disable-line no-array-constructor
+const pages = new Array(10).fill(null) // eslint-disable-line no-array-constructor
+const entries = new Array(10).fill(null) // eslint-disable-line no-array-constructor
 
 module.exports = pages
   .map(() => entries.map(() => ({
