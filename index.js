665787edf0beaf6c22e101cdeb8bdc36fd67e962
Falk Hoppe <falkhoppe81@gmail.com>
2017-02-05 10:06:41 +0100

    add product pagination

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const morgan = require('morgan')
 const products = require('./products')
 
 const app = express()
+
 app.use(morgan('combined'))
 
 app.set('view engine', 'pug')
 
 app.get('/products', (req, res) => {
-  const page = req.query.page
-  res.status(200).render('product-list', { products: products[page] })
+  const page = (req.query.page && parseInt(req.query.page)) || 0
+  res.status(200).render('product-list', { products: products[page], page, pageCount: products.length })
 })
 
 app.listen(3001)
