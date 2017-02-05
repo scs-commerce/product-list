285c61c9961e904f3ec7098c01ef23ab67a6158d
Falk Hoppe <falkhoppe81@gmail.com>
2017-02-05 12:53:09 +0100

    capture exceptions and deliver error pages

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ app.use(morgan('combined'))
 
 app.set('view engine', 'pug')
 
-app.get('/products', (req, res) => {
+app.get('/products', (req, res, next) => {
   const page = (req.query.page && parseInt(req.query.page)) || 0
   if (products[page]) {
     res.status(200).render('product-list', { products: products[page], page, pageCount: products.length })
   } else {
-    res.status(404).render('404')
+    next()
   }
 })
+
+app.use((req, res, next) =>
+  res.status(404).render('404'))
+
+app.use((error, req, res, next) =>
+  res.status(500).render('500', { error }))
 
 app.listen(3001)
